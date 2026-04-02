5fe6529efc1aa3c3ec3a6a5df041b60702c22ccd
Alessandro Pomponi <alescode1@gmail.com>
2026-04-02 14:52:14 -0400

    added foundation of frontend assignment functionality with database, but still doesn't work yet

--- FrontEnd/schedule/displayAssignments.js
+++ FrontEnd/schedule/displayAssignments.js
@@ -1,1 +1,46 @@
-// This will be for grabbing assignments from backend and then displaying them.
+// This is for grabbing assignments from backend and then displaying them.
+async function loadDashboardAssignments() {
+    const container = document.querySelector(".assignmentSection");
+    if (!container) return;
+
+    container.innerHTML = "<p>Loading assignments...</p>";
+
+    try {
+        const res = await fetch("http://localhost:3000/api/assignments/all");
+        if (!res.ok) throw new Error("Failed to fetch assignments");
+
+        const assignments =  await res.json();
+        container.innerHTML = "";
+
+        if (assignments.length === 0) {
+            container.innerHTML = "<p>No upcoming assignments!</p>";
+            return;
+        }
+
+        assignments.array.forEach(a => {
+            const dueDateString = a.dueDate ? new Date(a.dueDate).toLocaleDateString() : "No due date";
+
+            const box = document.createElement("div");
+            box.className = "assignmentBox";
+
+            box.innerHTML = `
+                <div class="assignmentText">
+                    <h4>Due: ${dueDateString}</h4>
+                    <h5>${a.title}</h5>
+                    <p>Course ID: ${a.courseId}</p>
+                </div>
+                <label>
+                    Completed <input type="checkbox" class="dashboard-checkbox" data-id="${a.id}">
+                </label>            
+            `;
+            container.appendChild(box);
+        });
+    } catch (error) {
+        console.error("Error loading dashboard assingments: ", error);
+        container.innerHTML = "<p>Error loading assignments.</p>";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadDashboardAssignments)
+
+
