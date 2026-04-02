0bcaeb96176548334a5febf2425f59a5578829b6
Joshua <joshuaindig@gmail.com>
2026-04-02 12:20:43 -0400

    fixed error that prevented the server from starting

--- BackEnd/routes/assignmentRoutes.js
+++ BackEnd/routes/assignmentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const assignmentController = require("../controller/assignmentController");
 
 router.post("/add", assignmentController.addAssignment);
-router.get("/course/:courseId", assignmentController.getAssignmentByCourse);
+router.get("/course/:courseId", assignmentController.getAssignmentsByCourse);
 router.post("/toggle-completion", assignmentController.toggleCompletion);
 
 module.exports = router;
